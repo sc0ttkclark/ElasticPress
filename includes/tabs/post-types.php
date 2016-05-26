8b6596d258b18c1e45fab2fa29b816917be4f1e0
Chris Wiegman <chris.wiegman@10up.com>
2016-05-26 13:54:55 -0400

    Add simple form for checking post types

--- includes/tabs/post-types.php
+++ includes/tabs/post-types.php
@@ -1,1 +1,25 @@
 <?php
+/**
+ * Form for setting post type preferences
+ *
+ * @since   2.0
+ *
+ * @package elasticpress
+ *
+ * @author  Chris Wiegman <chris.wiegman@10up.com>
+ */
+
+// Set form action.
+$action = 'options.php';
+?>
+
+<form method="POST" action="<?php echo esc_attr( $action ); ?>">
+
+	<?php
+	settings_fields( 'elasticpress_post_types' );
+	do_settings_sections( 'elasticpress_post_types' );
+	?>
+
+	<input type="submit" class="button secondary button-large" value="<?php echo esc_attr__( 'Submit', 'elasticpress' ); ?>">
+
+</form>
